944925a3bf545fcdae961a99b7976d31305983c3
lizpeng.tw <lizpeng.tw@gmail.com>
2020-05-29 15:37:02 -0500

    style the upload form

--- vue-image-upload/src/components/Upload.vue
+++ vue-image-upload/src/components/Upload.vue
@@ -1,5 +1,8 @@
 <template>
-	<div>Upload</div>
+	<div class="dropper">
+		<input type="file" name="">
+		<span>Drag and drop image here to upload</span>
+	</div>
 </template>
 
 <script>
@@ -7,3 +10,26 @@ export default {
 	name: 'Upload'
 }
 </script>
+
+<style scoped>
+.dropper {
+	height: 30vh;
+	border: 2px dashed #000;
+	border-radius: 5px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+}
+
+.dropper:hover {
+	background-color: #eee;
+}
+
+input {
+	width: 100%;
+	height: 30vh;
+	position: absolute;
+	opacity: 0;
+}
+</style>
